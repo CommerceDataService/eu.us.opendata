0da3f682419eeef1b2f2b8b59fd4f166317d9c62
Jeff Chen <jchen85@gmail.com>
2016-10-16 22:34:00 -0400

    Auto reshape
    - function to convert long-form output to wide form, auto detection for
    GEO and IND

---
+++ wide.R
@@ -0,0 +1,30 @@
+#' Reshape datasets into wide form
+#' Auto detect if just GEO x TIME or GEO x IND x TIME
+#' 
+#' @param term 	 ID of the statistic requested 
+#' @import data.table 
+#' @export 
+
+wide <- function(df){
+  
+  if(!"IND" %in% colnames(df)){
+    print("Converting from long to wide GEO")
+    input <- df[,c("GEO_NAME","TIME","OBS_VALUE"),with=FALSE]
+    out <- reshape(input, 
+                   timevar = "GEO_NAME",
+                   idvar = c("TIME"),
+                   direction = "wide")
+    out$TIME <- as.numeric(as.character(out$TIME))
+    return(out)
+  } else if(sum(c("GEO","IND") %in% colnames(df))==2) {
+    print("Converting from long to wide GEO x INDUSTRY")
+    input <- df[,c("GEO_NAME","TIME","IND","OBS_VALUE"),with=FALSE]
+    out <- reshape(input, 
+                   timevar = c("GEO_NAME","IND"),
+                   idvar = c("TIME"),
+                   direction = "wide")
+    out$TIME <-  as.numeric(as.character(out$TIME))
+    return(out)
+  }
+}
+
