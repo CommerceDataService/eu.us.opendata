81d53a1ab7d1193671a35cf07b0b0f759dd8ed04
Andrea Julca <andrea.julca@gmail.com>
2016-10-21 19:17:50 -0400

    Start metadata cache update process building

---
+++ R/updateLocalRel.r
@@ -0,0 +1,12 @@
+#' Update local relationship metadata cache
+#' 
+#' @return Local cache of relationship metadata, as data.table
+#' @import data.table
+#' @export 
+
+updateLocalRel <- function(uid, pwd){
+
+
+		
+}
+
---
+++ R/updateLocalStruc.r
@@ -0,0 +1,10 @@
+#' Update local structure metadata cache
+#' 
+#' @return Local cache of structure metadata, as data.table
+#' @import data.table
+#' @export 
+
+updateLocalStruc <- function(uid, pwd){
+		
+}
+
