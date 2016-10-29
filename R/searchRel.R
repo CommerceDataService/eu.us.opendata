05a52d0eb9aec3fe8ee60fb7237a93c1a07e629a
Jeff Chen <jchen85@gmail.com>
2016-10-29 15:44:05 -0400

    Improvements to search
    Refined and standardized search should now work

--- R/searchRel.R
+++ R/searchRel.R
@@ -1,50 +1,77 @@
 #' Search Metadata Store Relationship Table using text search
 #' 
-#' @param asHtml  Option to render results in an interactive DT
+#' @param asHTML  Option to render results in an interactive DT
 #' @param term  Search term
 #' @import DT RCurl
 #' @export 
 
-searchRel <- function(term, asHtml = FALSE){
+searchRel <- function(term, asHTML = FALSE){
 	requireNamespace('DT', quietly = TRUE)
 	requireNamespace('RCurl', quietly = TRUE)
 	
-		eu.us.opendata::updateCache();
+	#	eu.us.opendata::updateCache();
 
     flag <- c()
     
     #Check if there is any term that is passed into the function
     if(nchar(trimws(term))!= 0){
-        split <- unlist(strsplit(gsub("[[:punct:]]","",term),"[[:space:]]"))
+        
         
         #Step one -- canonical, RelTable table will go here -- will loadLocalRel.r run at some point before?
           localRel <- loadLocalRel()
-          data <- as.data.frame(localRel[, 1:2, with = FALSE])
+          data <- as.data.frame(localRel[,, with = FALSE])
+          data <- data[!duplicated(data[,2]),]
+        
+        #Step two: synonym engine
+          split <- searchRefine(term)
           
-        #Step two: Scrapped pure regex in this round
+        #Step three: Searching
           for(i in 0:5){
             for(k in split){
-              flag <- c(flag,
-                        data[agrep(k, data[,2], max.distance = i, ignore.case = TRUE),2])
+              for(j in 2:13){
+                flag <- c(flag,
+                          as.vector(
+                            data[agrep(k, data[,j], max.distance = i, ignore.case = TRUE),2]
+                            )
+                          )
+                  }
+              
             }
           }
     }
       
+    print("ok")
     #Check if there are any results
+		
     if(length(flag) == 0){
         print("Search: No matches")
         return(data.frame())
-      } else {
       
+      } else if(length(flag)>0 & asHTML==FALSE){
       #recommended relative rankings
         results <- data.frame((table(flag)))
         results$series <- (as.character(results$flag))
-        results$Rel_score <- 100*results$Freq/max(results$Freq)
+        results$Rel_score <- round(100*results$Freq/max(results$Freq),2)
         
         results <- merge(data,results,by.x="Rel_name",by.y="series")
         results <- results[,c("Rel_score","Rel_ID","Rel_name")]
+        results <- results[!duplicated(results),]
         results <- results[order(-results$Rel_score),]
+        return(results)
         
-      return(results)
-      }
+      } else if(length(flag)>0 && asHTML==TRUE){
+        
+        #recommended relative rankings
+        results <- data.frame((table(flag)))
+        results$series <- (as.character(results$flag))
+        results$Rel_score <- round(100*results$Freq/max(results$Freq),2)
+        
+        results <- merge(data,results,by.x="Rel_name",by.y="series")
+        results <- results[,c("Rel_score","Rel_ID","Rel_name")]
+        results <- results[!duplicated(results),]
+        results <- results[order(-results$Rel_score),]
+		    datatable(data.frame(results))
+		} 
+		
+	
 }
